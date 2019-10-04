f89cd7be6ef401f5dc47e29c90b1a6889fb9dd5e
hlerda1 <horacio.lerda@gmail.com>
2019-10-04 20:28:43 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
+//import React, {Component} from 'react';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    titulo: 'mi trabajo de mcga'
+  }
+
+  componentWillMount = () => {
+    console.log('se esta por montar/dibujar el componente');
+  }
+
+  componentDidMount= () => {
+    console.log('Ya se dibujo el componente');
+  }
+
+  componentWillReceiveProps(nextProps, nextState) {
+    console.log('Se actualizó el componente');
+    console.log(nextState);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('state actual', this.state);
+    console.log('state actualizado', nextState);
+    return false;
+  }
+
+  render() {
+    console.log('Se esta dibujando el componente');
+    return(
+      <div>
+        <button onClick={()=>{
+          this.setState({titulo: 'Nuevo Titulo'})
+        }}>
+        Cambiar Titulo
+        </button>
+        <Card text={this.state.titulo} />
+      </div>
+    )
+  }
 }
+
+
+  // <div>{this.state.titulo}</div>
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//         Adios Mundo
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/card.js
@@ -0,0 +1,13 @@
+//Componente funcional
+import React from 'react'
+
+const Card = (props) => {
+  console.log(props);
+  return (
+    <div>
+      {props.text}
+    </div>
+  )
+}
+
+export default Card
