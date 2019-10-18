de9ec61d004ee023e8a2a3e6dc2036e1b4cc4156
hlerda1 <horacio.lerda@gmail.com>
2019-10-18 20:42:48 -0300

    pagina de login reactiva

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
 //import React, {Component} from 'react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Card from './card'
+//import logo from './logo.svg';
+//import './App.css';
+//import Card from './card'
+import Login from './components/login'
 
-class App extends React.Component {
-
-  state = {
-    titulo: 'mi trabajo de mcga'
-  }
-
-  componentWillMount = () => {
-    console.log('se esta por montar/dibujar el componente');
-  }
-
-  componentDidMount= () => {
-    console.log('Ya se dibujo el componente');
-  }
-
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log('Se actualizó el componente');
-    console.log(nextState);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('state actual', this.state);
-    console.log('state actualizado', nextState);
-    return false;
-  }
-
-  render() {
-    console.log('Se esta dibujando el componente');
-    return(
-      <div>
-        <button onClick={()=>{
-          this.setState({titulo: 'Nuevo Titulo'})
-        }}>
-        Cambiar Titulo
-        </button>
-        <Card text={this.state.titulo} />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
+
+export default App;
+
+
+// class App extends React.Component {
+//
+//   state = {
+//     titulo: 'mi trabajo de mcga'
+//   }
+//
+//   componentWillMount = () => {
+//     console.log('se esta por montar/dibujar el componente');
+//   }
+//
+//   componentDidMount= () => {
+//     console.log('Ya se dibujo el componente');
+//   }
+//
+//   componentWillReceiveProps(nextProps, nextState) {
+//     console.log('Se actualizó el componente');
+//     console.log(nextState);
+//   }
+
+  ////Este componenete no permite el re-renderizado, por lo que el boton no funcionaria
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('state actual', this.state);
+  //   console.log('state actualizado', nextState);
+  //   return false;
+  // }
+
+//   render() {
+//     console.log('Se esta dibujando el componente');
+//     return(
+//       <div>
+//         <button onClick={()=>{
+//           this.setState({titulo: 'Nuevo Titulo'})
+//         }}>
+//         Cambiar Titulo
+//         </button>
+//         <Card text={this.state.titulo} />
+//       </div>
+//     )
+//   }
+// }
 
 
   // <div>{this.state.titulo}</div>
@@ -68,4 +81,4 @@ class App extends React.Component {
 //   );
 // }
 
-export default App;
+// export default App;
